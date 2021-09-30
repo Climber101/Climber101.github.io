062966d66cdb750bbe93f6e16bbfd4dd4472ffc6
Climber101 <90599812+Climber101@users.noreply.github.com>
2021-09-30 20:28:46 +1000

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
 
 
 $(document).ready(function () {
-    $("article").hide();
-    $("#article1").show();
     
     $("#menu1").click(function () {
-        $("article").hide();
-        $("#article1").show();
+        $("article").show();
     });
     
     $("#menu2").click(function () {
         $("article").hide();
+        $("#article1").show();
         $("#article2").show();
     });
     
     $("#menu3").click(function () {
         $("article").hide();
+        $("#article1").show();
         $("#article3").show();
     });
     
     $("#menu4").click(function () {
         $("article").hide();
+        $("#article1").show();
         $("#article4").show();
     });
 });
